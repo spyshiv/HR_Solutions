b7817348ded18180ccd94aac5ffb9dc3398de994
Shiv Baran Singh <Shiv Baran Singh>
2015-10-22 10:01:04 +0530

    Added:Algorithm-->Implementation-->FindDigits

---
+++ In C++/Algorithms/Implementation/FindDigits.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+int main()   
+{  
+      int t;  
+      int M,N,count,rem;  
+      cin>>t;  
+      while(t--)  
+      {  
+           cin>>N;  
+           M=N;  
+           count=0;  
+           while(N>0)  
+           {  
+                rem=N%10;  
+                if(rem!=0)  
+                if(M%rem==0)  
+                count++;  
+                N=N/10;  
+           }  
+           cout<<count<<endl;   
+      }  
+return 0;  
+} 
